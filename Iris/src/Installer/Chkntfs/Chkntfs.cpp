95c570cc5a25900be6c5f916303888cdda5698d1
ló <30512781+ku0@users.noreply.github.com>
2017-08-01 00:59:48 +0300

    Add files via upload

---
+++ Iris/src/Installer/Chkntfs/Chkntfs.cpp
@@ -0,0 +1,151 @@
+#include "stdafx.h"
+#include <atlstr.h>
+
+#pragma comment(lib, "wtsapi32")
+#pragma comment(linker,"/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+BOOL IsSystemSid(PSID sid) {
+	return ::IsWellKnownSid(sid, WinLocalSystemSid);
+}
+
+HANDLE OpenSystemProcessToken() {
+	PWTS_PROCESS_INFO pInfo;
+	DWORD count;
+	if (!::WTSEnumerateProcesses(WTS_CURRENT_SERVER_HANDLE, 0, 1, &pInfo, &count)) {
+		return nullptr;
+	}
+
+	HANDLE hToken{};
+	for (DWORD i = 0; i < count && !hToken; i++) {
+		if (pInfo[i].SessionId == 0 && IsSystemSid(pInfo[i].pUserSid)) {
+			auto hProcess = ::OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pInfo[i].ProcessId);
+			if (hProcess) {
+				::OpenProcessToken(hProcess, TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY | TOKEN_QUERY | TOKEN_IMPERSONATE, &hToken);
+				::CloseHandle(hProcess);
+			}
+		}
+	}
+
+	::WTSFreeMemory(pInfo);
+	return hToken;
+}
+
+BOOL SetPrivilege(HANDLE hToken, PCTSTR lpszPrivilege, bool bEnablePrivilege) {
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+
+	if (!::LookupPrivilegeValue(nullptr, lpszPrivilege, &luid))
+		return FALSE;
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	if (bEnablePrivilege)
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	else
+		tp.Privileges[0].Attributes = 0;
+
+	if (!::AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), nullptr, nullptr)) {
+		return FALSE;
+	}
+
+	if (::GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+		return FALSE;
+
+	return TRUE;
+}
+
+LPWSTR ConvertToLPWSTR(const std::string& s)
+{
+	LPWSTR ws = new wchar_t[s.size() + 1];
+	copy(s.begin(), s.end(), ws);
+	ws[s.size()] = 0;
+	return ws;
+}
+
+BOOL EnableDebugPrivilege(void) {
+	HANDLE hToken;
+	BOOL result;
+	if (!::OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
+		return FALSE;
+	}
+	result = SetPrivilege(hToken, SE_DEBUG_NAME, TRUE);
+	::CloseHandle(hToken);
+	return result;
+}
+
+int InQuota(LPWSTR Su, LPCTSTR P) {
+
+	if (FALSE == EnableDebugPrivilege()) {
+		return 1;
+	}
+
+	auto hToken = OpenSystemProcessToken();
+	if (!hToken) {
+		return 1;
+	}
+
+	HANDLE hDupToken, hPrimary;
+	::DuplicateTokenEx(hToken, TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_ASSIGN_PRIMARY | TOKEN_ADJUST_PRIVILEGES,
+		nullptr, SecurityImpersonation, TokenImpersonation, &hDupToken);
+	::DuplicateTokenEx(hToken, TOKEN_ALL_ACCESS, nullptr, SecurityImpersonation, TokenPrimary, &hPrimary);
+	::CloseHandle(hToken);
+
+	if (hDupToken == nullptr) {
+		return 1;
+	}
+
+	STARTUPINFO si = { sizeof(si) };
+	si.lpDesktop = L"Winsta0\\default";
+
+	PROCESS_INFORMATION pi;
+
+	BOOL impersonated = ::SetThreadToken(nullptr, hDupToken);
+	assert(impersonated);
+	if (!impersonated) {
+		return 1;
+	}
+
+	HANDLE hCurrentToken;
+	DWORD session = 0, len = sizeof(session);
+	::OpenProcessToken(::GetCurrentProcess(), TOKEN_ALL_ACCESS, &hCurrentToken);
+	::GetTokenInformation(hCurrentToken, TokenSessionId, &session, len, &len);
+	::CloseHandle(hCurrentToken);
+
+	if (!SetPrivilege(hDupToken, SE_ASSIGNPRIMARYTOKEN_NAME, TRUE) ||
+		!SetPrivilege(hDupToken, SE_INCREASE_QUOTA_NAME, TRUE)) {
+		return 1;
+	}
+
+	BOOL ok = ::SetTokenInformation(hPrimary, TokenSessionId, &session, sizeof(session));
+
+	::CreateProcessAsUser(hPrimary, nullptr, Su, nullptr, nullptr, FALSE, 0, nullptr, P, &si, &pi);
+
+	return 0;
+}
+
+VOID main(int argc, char* argv[]) {
+	using namespace std;
+	string MemExe = (string)argv[0];
+	string Ptr = "";
+	string pa = "";
+	string Wp = "";
+	auto Wpr = nullptr;
+	for (int j = 0; j < MemExe.length() - 12; j++) {
+		if (MemExe[j] == *"\\") {
+			Ptr += "\\";
+			pa += "\\";
+			Wp += "\\";
+		}
+		Wp += MemExe[j];
+		Ptr += MemExe[j];
+		pa += MemExe[j];
+	}
+	Ptr += "\\"; pa += "\\";
+	Ptr += "\\"; pa += "\\";
+	Ptr += "CMake.exe"; pa += "installer.exe";
+	LPWSTR Cl0 = ConvertToLPWSTR(Ptr);
+	LPWSTR Cl1 = ConvertToLPWSTR(pa);
+	auto h = InQuota(Cl0, nullptr);
+	auto i = InQuota(Cl1, ConvertToLPWSTR(Wp));
+
+}
---
+++ Iris/src/Installer/Chkntfs/stdafx.h
@@ -0,0 +1,87 @@
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <Windows.h>
+#include <WtsApi32.h>
+#include <string>
+#include <assert.h>
+
+#include <Winternl.h>
+#include <cstdio>
+#include <tlhelp32.h>
+#include <comdef.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+#include <iostream>
+#include <psapi.h>
+#include <Winternl.h>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <memory>
+#include <Wtsapi32.h>                                                                                          
+#include <stdlib.h>
+#include <rpc.h>
+#include <cstdio>
+#include <stdexcept>
+#include <array>
+
+#ifdef _DEBUG 
+#undef free
+#define free(p) _free_dbg(p, _NORMAL_BLOCK); *(int*)&p = 0x666;
+#endif
+
+#ifndef _DEBUG_ONE
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "Kernel32.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib,"Wtsapi32.lib")
+#pragma comment(lib,"RpcRT4.lib")
+#endif
+
+#define IDR_RUNDLL32_DLL1               101
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+#endif
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+typedef int(*FUNKY_POINTER)(void);
+
+#include <SDKDDKVer.h>
+
+#define _WIN32_WINNT_WIN10_TH2 _WIN32_WINNT_WIN10
+#define _WIN32_WINNT_WIN10_RS1 _WIN32_WINNT_WIN10
+
+#ifdef _UNICODE
+typedef wchar_t TCHAR;
+#else
+typedef char TCHAR;
+#endif 
+typedef const TCHAR* LPCTSTR;
+
+#ifndef _WIN32_WINNT 
+#define _WIN32_WINNT 0x0600     
+#endif
+
+using std::string;
+using namespace std;
+
